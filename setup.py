5c858381b7ce0ae5035ce76d0b381be5c59155c2
Mona Buisson-Fenet <buisson-fenet@is.mpg.de>
2019-06-27 16:48:15 +0200

    More corrections on finding assets

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_modified_InvertedDoublePendulum_v2/',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
+      install_requires=['gym', 'os']  # And any other dependencies foo needs
 )

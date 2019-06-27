befe59d1b9fef56dc54863bf9fcc625cdb5b3091
monabf <26089777+monabf@users.noreply.github.com>
2019-06-27 15:43:22 +0200

    Added import of MujocoEnv for MuJoCo missing error

--- gym_modified_InvertedDoublePendulum_v2/envs/__init__.py
+++ gym_modified_InvertedDoublePendulum_v2/envs/__init__.py
@@ -1,1 +1,4 @@
+from gym.envs.mujoco.mujoco_env import MujocoEnv
+# ^^^^^ so that user gets the correct error
+# message if mujoco is not installed correctly
 from gym_modified_InvertedDoublePendulum_v2.envs.modified_inverteddoublependulum import ModifiedInvertedDoublePendulumEnv
